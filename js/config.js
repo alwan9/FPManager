b7893894dac056dbfa03cb8e1146918708377291
alwan9 <hafizsusilo86@gmail.com>
2026-06-28 22:45:55 +0700

    update config for live mode

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-  MOCK_MODE: true, // Ubah ke false untuk menghubungkan ke Google Sheets API secara langsung
-  API_URL: 'https://script.google.com/macros/s/YOUR_SCRIPT_ID/exec',
+  MOCK_MODE: false, // Ubah ke false untuk menghubungkan ke Google Sheets API secara langsung
+  API_URL: 'https://script.google.com/macros/s/AKfycbyWlrsELLiNlzOIDi-snTQD1m17kS_Bw5y3Izo04y0ixetgXlTdtd4n75xg2E2_eAcDwg/exec',
   WA_TEMPLATE: 'gimana kak? apakah sudah sesuai? atau bagai mana ya kak?',
   ADMIN_PASS: 'admin123' // password login default
 };
